56a5672bee483f3a9bafdf89b7222647e243db28
Taylor C. MacDonald <taylor@helloample.com>
2020-08-24 08:23:50 -0400

    Execute during onBuild life-cycle event

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Parser = require("./lib/parser")
 
 module.exports = {
-  onPostBuild: async ({ utils, inputs }) => {
+  onBuild: async ({ utils, inputs }) => {
     const parser = new Parser(inputs.source, inputs.destination, inputs.defaultBranch, utils)
     await parser.perform()
     const stats = parser.stats()
